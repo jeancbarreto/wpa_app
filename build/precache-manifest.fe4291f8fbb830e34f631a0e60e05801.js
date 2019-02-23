25f8f7207bedb032aeddd4903b574bbc4afebf22
Jean Barreto <jbarreto@verticallsoft.cl>
2019-02-23 19:48:32 -0300

    cambios

---
+++ build/precache-manifest.fe4291f8fbb830e34f631a0e60e05801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9ac69ee34851867883ed",
+    "url": "/static/js/main.9ac69ee3.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "9ac69ee34851867883ed",
+    "url": "/static/css/main.17bcdb37.chunk.css"
+  },
+  {
+    "revision": "8fdc61a87ae78f897bfa351393dee466",
+    "url": "/index.html"
+  }
+];
